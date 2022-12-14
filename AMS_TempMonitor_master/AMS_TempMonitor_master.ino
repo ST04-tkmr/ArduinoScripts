1714cdd2bc59d66b9ebb7a65563c2ba776baeeee
ST04-tkmr <84787828+ST04-tkmr@users.noreply.github.com>
2022-12-15 01:44:32 +0900

    ver4.beta
    マスターとスレーブ、分かりやすくファイル名変更。ファイル作っただけで機能は分けていない。

---
+++ AMS_TempMonitor_master/AMS_TempMonitor_master.ino
@@ -0,0 +1,20 @@
+#include "AMS_TemperatureMonitoring.h"
+#include <MsTimer2.h>
+#include <Wire.h>
+
+#define PROMINI_ADRS0 0b0000000
+#define PROMINI_ADRS1 0b0000001
+#define PROMINI_ADRS2 0b0000010
+#define PROMINI_ADRS3 0b0000100
+#define PROMINI_ADRS4 0b0001000
+#define PROMINI_ADRS5 0b0010000
+#define PROMINI_ADRS6 0b0100000
+#define PROMINI_ADRS7 0b1000000
+
+void setup() {
+  Wire.begin();
+}
+
+void loop() {
+
+}
