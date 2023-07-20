22a033146c48cd8beafb00a0de08117fece8d458
ST04-tkmr <84787828+ST04-tkmr@users.noreply.github.com>
2023-07-20 18:32:03 +0900

    io設定追加

--- InverterController_ver4/src/main.cpp
+++ InverterController_ver4/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
+#include <MsTimer2.h>
+#include "Inverter.hpp"
+#include "Inverter_dfs.hpp"
+#include "Accel.hpp"
+#include "Switch.hpp"
+#include "IO_dfs.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+Inverter *inverter;
+Accel *accel;
+unsigned short val[2];
+float torque;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
